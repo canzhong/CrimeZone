ada55d027ff9c36f0ee8bc652b439b15a935c002
aminPhakhruddin <amin.phakhruddin@gmail.com>
2019-08-11 14:47:34 -0500

    Updated the event register activity to push latitude and longitude to the database.

--- app/src/main/java/com/example/crimezone/EventRegisterActivity.java
+++ app/src/main/java/com/example/crimezone/EventRegisterActivity.java
@@ -3,16 +3,22 @@ package com.example.crimezone;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 
 public class EventRegisterActivity extends AppCompatActivity {
@@ -37,7 +43,7 @@ public class EventRegisterActivity extends AppCompatActivity {
 
         mToolbar = (Toolbar) findViewById(R.id.settings_app_bar);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("Account Settings");
+        getSupportActionBar().setTitle("Register Event");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Firebase
@@ -62,12 +68,18 @@ public class EventRegisterActivity extends AppCompatActivity {
                 String name = mNameInput.getEditText().getText().toString();
                 String address = mAddressInput.getEditText().getText().toString();
 
+                LatLng myLatLng = getLocationFromAddress(getApplicationContext(), address);
+                String latStr = String.valueOf(myLatLng.latitude);
+                String lngStr = String.valueOf(myLatLng.longitude);
+
 
                 String eventId = name;
                 mEventsDatabase = FirebaseDatabase.getInstance().getReference().child("Events").child(eventId);
 
                 mEventsDatabase.child("name").setValue(name);
                 mEventsDatabase.child("address").setValue(address);
+                mEventsDatabase.child("latitude").setValue(myLatLng.latitude);
+                mEventsDatabase.child("longitude").setValue(myLatLng.longitude);
 
                 Toast.makeText(EventRegisterActivity.this, "Event created", Toast.LENGTH_LONG).show();
 
@@ -86,4 +98,30 @@ public class EventRegisterActivity extends AppCompatActivity {
         }
         return randomStringBuilder.toString();
     }
+
+    public LatLng getLocationFromAddress(Context context, String strAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+
+            Address location = address.get(0);
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+        }
+
+        return p1;
+    }
 }
+
+
