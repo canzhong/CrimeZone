050b900bd1806a04569b3f40538021fdcc4294cf
Zhong Can <zhongcan00@gmail.com>
2019-08-13 10:48:42 -0500

    started view profile activity, created an api for testing purposes

--- app/src/main/java/com/example/crimezone/MapsActivity.java
+++ app/src/main/java/com/example/crimezone/MapsActivity.java
@@ -2,8 +2,10 @@ package com.example.crimezone;
 
 import androidx.annotation.NonNull;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Handler;
 import android.util.Log;
@@ -15,6 +17,18 @@ import androidx.core.content.ContextCompat;
 import androidx.core.app.ActivityCompat;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -28,6 +42,8 @@ import android.location.Location;
 
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -45,16 +61,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
-
+public class MapsActivity extends FragmentActivity implements
+        GoogleMap.OnMarkerClickListener,
+        OnMapReadyCallback,
+        GoogleMap.OnInfoWindowClickListener
+        {
 
 
     private GoogleMap mMap;
     private UiSettings mUiSettings;
     private Circle mCircle;
     private LatLng currentLoc;
-    private LocationManager mLocationManager;
-    private android.location.LocationListener mLocationListener;
 
     private static boolean mLocationPermissionGranted;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 100;
@@ -64,21 +81,58 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
     private static final LatLng mDefaultLocation = new LatLng(-34, 151);;
     private DatabaseReference mDatabase;
 
-
-
     private HashMap<Marker, Events2> eventsHashMap = new HashMap<Marker, Events2>();
+
+    private static final String TAG = "MapActivity";
+    private static final long INTERVAL = 1000 * 10;
+    private static final long FASTEST_INTERVAL = 1000 * 5;
+    LocationRequest locationRequest;
+    private boolean requestingLocationUpdates;
+    private LocationCallback locationCallback;
+    private Location pLocation;
+    private LocationManager mLocationmanager;
+    private LocationListener mLocationListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                requestingLocationUpdates = true;
+                createLocationRequest();
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+
+
+
 
     }
 
@@ -107,9 +161,94 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
         mMap.setOnMarkerClickListener(this);
         mMap.setOnInfoWindowClickListener(this);
 
+        mLocationmanager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        mLocationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                mCircle.remove();
+                mCircle = mMap.addCircle(new CircleOptions()
+                        .center(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()))
+                        .radius(1609)
+                        .strokeColor(Color.TRANSPARENT)
+                        .fillColor(0x25ff9999));
+
+            }
+
+            public void onStatusChanged(String provier, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    mMap.getUiSettings();
+                }
+            }
+
+        };
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            mLocationPermissionGranted = true;
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                mCircle = mMap.addCircle(new CircleOptions()
+                                        .center(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()))
+                                        .radius(1609)
+                                        .strokeColor(Color.TRANSPARENT)
+                                        .fillColor(0x25ff9999));
+                            }
+                        }
+                    });
+
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
 
     }
 
+    protected void createLocationRequest() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (requestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+            Log.d(TAG, "Location update started ..............: ");
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
 
 
     private void setUserInterfaceControls() {
@@ -206,8 +345,6 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
                                         .strokeColor(Color.TRANSPARENT)
                                         .fillColor(0x25ff9999)
                                 );
-                            } else {
-                                mCircle.setCenter(currentLoc);
                             }
 
                         } else {
@@ -223,6 +360,10 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
 
     }
 
+    protected void updateView() {
+
+
+    }
 
     private void displayMarkers(){
 
